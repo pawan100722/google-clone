fa7135e6246dbd1a3a657a333303a4c8fc9ab3ff
pawan100722 <pawan100722@gmail.com>
2025-04-25 16:01:00 +0530

    added basic UI

---
+++ src/components/MainComponent.tsx
@@ -0,0 +1,9 @@
+import { SearchBox } from "./SearchBox";
+import '../css/MainComponent.css';
+
+export const MainComponent=()=>{
+
+  return <div className="main-component-container">
+    <SearchBox/>
+  </div>
+}
---
+++ src/components/SearchBox.tsx
@@ -0,0 +1,20 @@
+import "../css/SearchBox.css";
+import micIcon from "../icons/MicIcon.svg";
+import searchIcon from "../icons/SearchIcon.svg";
+import cameraIcon from "../icons/CameraIcon.svg";
+
+export const SearchBox = () => {
+  return (
+    <div className="search-main-container">
+      <h1 className="search-main-text">Google</h1>
+      <div className="search-container">
+        <img className="search-icon" src={searchIcon} alt="" />
+        <input type="text" className="search-box" />
+        <div className="icons-container">
+          <img className="search-icon" src={micIcon} alt="" />
+          <img className="search-icon" src={cameraIcon} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
